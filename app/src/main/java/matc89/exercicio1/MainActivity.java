bdbffb21713ec86a877db2a7d2c48d52daa2c7ee
mirandafabio <45582010+mirandafabio@users.noreply.github.com>
2019-09-08 20:51:29 -0300

    Após o teste!

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,12 +2,34 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String texto = "Mundo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.start();
+    }
+
+    private void start() {
+
+        TextView textView = (TextView) findViewById(R.id.labelMensagem);
+        textView.setText("Alô, " + MainActivity.texto + "!");
+
+    }
+
+    public void mudar_texto(View view){
+
+        EditText editText = (EditText) findViewById(R.id.editNome);
+        MainActivity.texto = editText.getText().toString();
+
+        this.start();
     }
 }
